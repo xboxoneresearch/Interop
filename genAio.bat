365e61b5df8dd6f39c2cb2a3404480d0e9e6fc04
tuxuser <462620+tuxuser@users.noreply.github.com>
2024-08-04 07:30:06 +0200

    fix: Typo in genAio.bat

--- genAio.bat
+++ genAio.bat
@@ -1,4 +1,4 @@
 type src\*.cs > aio.cs
 type src\00_GlobalUsings.cs src\01_Interop.cs src\XCrd.cs > xcrd.cs
 type src\00_GlobalUsings.cs src\01_Interop.cs src\LicenseManager.cs > license.cs
-type src/00_GlobalUsings.cs src\01_Interop.cs src\UserManager.cs > usermanager.cs
+type src\00_GlobalUsings.cs src\01_Interop.cs src\UserManager.cs > usermanager.cs
